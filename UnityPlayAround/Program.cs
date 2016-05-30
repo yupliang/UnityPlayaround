f12de72478d4659239c151b45e161d3c2a3e77bf
app-01 <yupeiliang110@163.com>
2016-05-30 16:55:24 +0800

    commit files

---
+++ UnityPlayAround/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace UnityPlayAround
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+			MainClass mc = new MainClass ();
+			foreach (var item in mc.SayHello()) {
+				Console.WriteLine (item);
+			}
+			foreach (var item in FilterWithYield ()) {
+				Console.WriteLine (item);
+			}
+			foreach (var item in FilterWithoutYield()) {
+				Console.WriteLine (item);
+			}
+		}
+
+		static List<int> GetInitialData() {
+			return new List<int> (){ 1, 2, 3, 4 };
+		}
+
+		static IEnumerable<int> FilterWithoutYield() {
+			List<int> result = new List<int> ();
+			foreach (int i in GetInitialData()) {
+				if (i > 2) {
+					result.Add (i);
+				}
+			}
+			return result;
+		}
+
+		static IEnumerable<int> FilterWithYield() {
+			foreach (int i in GetInitialData()) {
+				if (i > 2) {
+					yield return i;
+				}
+			}
+			yield break;
+			Console.WriteLine ("code not execute here");
+		}
+
+		public IEnumerable<string> SayHello (){
+			yield return "Hello";
+			yield return "World";
+			yield return "!";
+			yield break;
+		}
+	}
+}
